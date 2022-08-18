ccf3d15eb6b129517a0fbcbf2aa1614a1c43996a
Sam Koh <sam910615@gmail.com>
2022-08-18 14:48:49 +0800

    First Deployment

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import TutorialList from "./components/tutorial-list.component";
+import AddTutorial from "./components/add-tutorial.component";
+import Tutorial from "./components/tutorial.component";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand navbar-dark bg-dark">
+                    <h2 className="navbar-brand">
+                        Sam
+                    </h2>
+                    <div className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to={"/tutorials"} className="nav-link">
+                                Tutorials
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={"/add"} className="nav-link">
+                                Add
+                            </Link>
+                        </li>
+                    </div>
+                </nav>
+                <div className="container mt-3">
+                    <Routes>
+                        <Route path="/" element={<TutorialList />} />
+                        <Route path="/tutorials" element={<TutorialList />} />
+                        <Route path="/add" element={<AddTutorial />} />
+                        <Route path="/tutorials/:id" element={<Tutorial />} />
+                    </Routes>
+                </div>
+            </div>
+        );
+    }
+}
+export default App;
